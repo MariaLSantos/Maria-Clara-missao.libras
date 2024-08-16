595f472cc721fa8b11889be9442f3b8cc2dc35a4
MariaLSantos <168207983+MariaLSantos@users.noreply.github.com>
2024-08-16 14:36:35 -0300

    heuuiek

---
+++ script.js
@@ -0,0 +1,7 @@
+const caixaPrincipal = Document.querySelector(".caixa-principal");
+const caixaPerguntas = Document.querySelector(".caixa-perguntas");
+const caixaAlternativas = Document.querySelector(".caixa-alternativas");
+const caixaResultado = Document.querySelector(".caixa-resultado");
+const textoResultado = Document.querySelector(".texto-resultado");
+
+
